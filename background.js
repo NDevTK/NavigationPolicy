6534143408afc56314d8c02bc7773d85e7f7669a
NDevTK <31563761+NDevTK@users.noreply.github.com>
2022-04-24 16:58:08 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 let processLock = new Map();
 
 chrome.webNavigation.onCommitted.addListener(e => {
-  if (e.transitionType === 'auto_toplevel') return
   if (['typed', 'auto_bookmark'].includes(e.transitionType) {
     return processLock.set(e.tabId, e.processId);
   }
   if (processLock.has(e.tabId) && processLock.get(e.tabId) !== e.processId) return chrome.tabs.update({url: 'about:blank'}, e.tabId);
+  processLock.set(e.tabId, e.processId);
 });
